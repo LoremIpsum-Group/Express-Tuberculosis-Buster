8b9fea9d979c95f8d6d35fec54658a11a997a994
Paul Adrian Gernale <202111258@fit.edu.ph>
2024-04-27 13:37:34 +0800

    feat: Added login account verification
    Added login account verification. With a default email "doctor", and password "apple".

--- src/onboarding_portal/ETBX_onboarding_portal.py
+++ src/onboarding_portal/ETBX_onboarding_portal.py
@@ -3,8 +3,57 @@ from kivymd.uix.behaviors import HoverBehavior
 from kivymd.uix.textfield import MDTextField
 
 
+import sqlite3
+from kivy.lang import Builder
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+
 class ETBxApp(MDApp):
-    pass 
+    def build(self):
+            conn = sqlite3.connect('first_db.db')
+            c = conn.cursor()
+            c.execute("""CREATE TABLE IF NOT EXISTS customers (  
+                    email TEXT,
+                    password TEXT)
+                """)
+
+            #default user
+            c.execute(
+                    '''
+                    INSERT INTO customers (email, password)
+                    VALUES ("doctor", "apple")
+                    '''
+                )
+            conn.commit()
+            conn.close()
+            return Builder.load_file('etbx.kv') 
+	
+    def login(self): 
+            conn = sqlite3.connect('first_db.db')
+            c = conn.cursor()	
+            email = self.root.ids.email_input.text
+            password = self.root.ids.password_input.text
+
+            if email != '' and password != '':
+                c.execute('SELECT password FROM customers WHERE email =?', [email])
+                account = c.fetchone() 
+                conn.close()  
+                if account: 
+                    if account[0] == password: 
+                        popup = Popup(title='Test popup', content=Label(text='Login Successful'), auto_dismiss=False)
+                        popup.open()
+                    else: 
+                        popup = Popup(title='Test popup', content=Label(text='Invalid Password'), auto_dismiss=False)
+                        popup.open()
+                else:
+                    popup = Popup(title='Test popup', content=Label(text='Invalid Email'), auto_dismiss=False)
+                    popup.open()
+            else: 
+                popup = Popup(title='Test popup', content=Label(text='Please enter all the fields'), auto_dismiss=False)
+                popup.open() 
+    
+
+
 
 
 ETBxApp().run()
