9744492f87547855089017d3d262faaf11b2080e
Jasper <202110402@fit.edu.ph>
2024-04-27 08:34:37 +0800

    docs(Additional info): Docuementation for main dashboard
    Added documentation for each classes in created base ui for dashboard

--- src/main_dashboard.py/ETBX_main_dashboard.py
+++ src/main_dashboard.py/ETBX_main_dashboard.py
@@ -4,6 +4,20 @@ from kivymd.uix.screen import MDScreen
 from kivymd.uix.button import MDRaisedButton
 
 class MyButton(MDRaisedButton):
+    """
+    A custom button class that changes the system cursor to 'hand' when the mouse is over the button.
+
+    Inherits from MDRaisedButton. This means all instantiations of this button are designed like this.
+    Adjustable in the future
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(self, **kwargs): Initializes the MyButton object.
+        on_mouseover(self, window, pos): Changes the system cursor to 'hand' when the mouse is over the button.
+    """
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         Window.bind(mouse_pos=self.on_mouseover)
@@ -16,6 +30,20 @@ class MyButton(MDRaisedButton):
 
 
 class MainDashboard(MDApp):
+    """
+    The main dashboard of the application.
+
+    This class represents the main dashboard of the application. It inherits from the `MDApp` class provided by the KivyMD library.
+
+    Attributes:
+        theme_cls (ThemeManager): The theme manager for the application.
+        icon (str): The path to the application's icon file.
+
+    Methods:
+        build(): Builds the main dashboard user interface. Returns an MDScreen
+
+    """
+
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Blue"
