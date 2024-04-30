8b30c84bba18d58d16e22339c01e10ff237bfab2
Paul Adrian Gernale <202111258@fit.edu.ph>
2024-04-30 21:25:22 +0800

    feat(Create Function): Tried implementing switch screen from login to signup. Created an onboarding file that serves as a medium for both.
    Tried implementing switch screen from login to signup. Created an onboarding file that serves as a medium for both.

--- src/onboarding_portal/ETBX_onboarding_portal.py
+++ src/onboarding_portal/ETBX_onboarding_portal.py
@@ -1,63 +1,37 @@
-from kivy.uix.screenmanager import Screen
-from kivy.lang import Builder
-import sqlite3
-from kivy.lang import Builder
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-from kivy.clock import Clock
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from ETBX_login_portal import LoginScreen
+from ETBX_signup_portal import SignupScreen
+from kivymd.app import MDApp
 
 
-
-Builder.load_file('etbx_onboarding_portal.kv')
-
-
-class LoginScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        
-        conn = sqlite3.connect('first_db.db')
-        c = conn.cursor()
-        c.execute("""CREATE TABLE IF NOT EXISTS customers (  
-                email TEXT,
-                password TEXT)
-            """)
-        #default user
-        c.execute(
-                    '''
-                    INSERT INTO customers (email, password)
-                    VALUES ("doctor", "apple")
-                    '''
-                )
-        conn.commit()
-        conn.close()
+class MainApp(MDApp):
+    def build(self):
+        self.screen_manager = ScreenManager()
+        login_screen = LoginScreen(name='login')
+        self.screen_manager.add_widget(login_screen)
+        self.screen_manager.add_widget(SignupScreen(name='signup'))
+        return self.screen_manager
     
-    def login(self, screen_manager): 
-            conn = sqlite3.connect('first_db.db')
-            c = conn.cursor()	
-            email = self.ids.email_input.text
-            password = self.ids.password_input.text
+    def login(self, screen): 
+        self.screen_manager.get_screen('login').login(screen)
 
-            if email != '' and password != '':
-                c.execute('SELECT password FROM customers WHERE email =?', [email])
-                account = c.fetchone() 
-                conn.close()  
-                if account: 
-                    if account[0] == password: 
-                        print("Credentials Valid! Switching Screen")  
-                        def switch_screen(dt):
-                            app.root.screen_manager.current = 'signup'
-                        print("Credentials Valid! Switching Screen")
-                        Clock.schedule_once(switch_screen, 0.1)  
-                        # popup = Popup(title='Test popup', content=Label(text='Login Successful'), auto_dismiss=False)
-                        # popup.open()
-                    else: 
-                        popup = Popup(title='Test popup', content=Label(text='Invalid Password'), auto_dismiss=False)
-                        popup.open()
-                else:
-                    popup = Popup(title='Test popup', content=Label(text='Invalid Email'), auto_dismiss=False)
-                    popup.open()
-            else: 
-                popup = Popup(title='Test popup', content=Label(text='Please enter all the fields'), auto_dismiss=False)
-                popup.open()         
+if __name__ == '__main__':
+    MainApp().run()
+
+# from kivy.app import App
+# from kivy.uix.screenmanager import ScreenManager, Screen
+# from login import LoginScreen
+# from signup import SignupScreen
+# from kivymd.app import MDApp
 
 
+# class MainApp(MDApp):
+#     def build(self):
+#         self.screen_manager = ScreenManager()
+#         self.screen_manager.add_widget(LoginScreen(name='login'))
+#         self.screen_manager.add_widget(SignupScreen(name='signup'))
+#         return self.screen_manager
+
+# if __name__ == '__main__':
+#     MainApp().run()
