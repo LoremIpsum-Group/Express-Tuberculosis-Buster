bc1650c14f79e3dc4ba3c0db4522276469c3e582
Jasper <202110402@fit.edu.ph>
2024-04-30 20:41:07 +0800

    feat(Adjust UI): Adjusted repository and created widget transitions
    Created directory for only the main dashboard, reconfigured m_dashb into main_dashb.kv for widget transition feature, and  refactored main_dashboard.py
    
    Padding for the labels are an issue, cursor highlights immediately when hovered over area of the label

--- src/main_dashboard/ETBX_main_dashboard.py
+++ src/main_dashboard/ETBX_main_dashboard.py
@@ -1,10 +1,11 @@
 from kivy.core.window import Window
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRaisedButton
+from kivymd.uix.button import MDRaisedButton # not currently used
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
 
 from kivy.lang import Builder
 
-class MyButton(MDRaisedButton):
+class MyButton(MDRaisedButton): #not currently used
     """
     A custom button class that changes the system cursor to 'hand' when the mouse is over the button.
 
@@ -30,6 +31,23 @@ class MyButton(MDRaisedButton):
             Window.set_system_cursor('arrow')
 
 
+class MainMenu(Screen):
+    pass
+
+
+class ScanImage(Screen):
+    
+    pass
+
+
+class ViewRecords(Screen):
+    pass
+
+
+class ManageUsers(Screen):
+    pass
+
+
 class MainDashboard(MDApp):
     """
     The main dashboard of the application.
@@ -49,7 +67,22 @@ class MainDashboard(MDApp):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "Blue"
         self.icon = "assets\lung-temporary-logo.ico"
+        Builder.load_file("m_dashb_2.kv")
 
-        return Builder.load_file('m_dashb.kv')
+        self.screen_manager = ScreenManager(transition=NoTransition())
+        self.screen_manager.add_widget(MainMenu(name="main_menu"))
+        self.screen_manager.add_widget(ScanImage(name="scan_img"))
+        self.screen_manager.add_widget(ViewRecords(name="view_rec"))
+        self.screen_manager.add_widget(ManageUsers(name="manage_users"))
 
-MainDashboard().run()
+        return self.screen_manager
+
+
+def run_main_dashboard():
+    """
+    Runs the main dashboard of the application. For simple integration when switching panels
+    """
+    MainDashboard().run()
+
+if __name__ == "__main__":
+    run_main_dashboard()
