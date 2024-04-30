73b2c871d4cb8b923af0089f15978b5d3943a0e6
Paul Adrian Gernale <202111258@fit.edu.ph>
2024-04-30 21:06:28 +0800

    feat(Create Function): Tried adding a switch screen to signup, failed to do so.
    Tried adding a switch screen to signup, failed to do so.

--- src/onboarding_portal/ETBX_onboarding_portal.py
+++ src/onboarding_portal/ETBX_onboarding_portal.py
@@ -1,38 +1,41 @@
-from kivymd.app import MDApp 
-from kivymd.uix.behaviors import HoverBehavior
-from kivymd.uix.textfield import MDTextField
-
-
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
 import sqlite3
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+from kivy.clock import Clock
 
-class ETBxApp(MDApp):
-    def build(self):
-            conn = sqlite3.connect('first_db.db')
-            c = conn.cursor()
-            c.execute("""CREATE TABLE IF NOT EXISTS customers (  
-                    email TEXT,
-                    password TEXT)
-                """)
 
-            #default user
-            c.execute(
+
+Builder.load_file('etbx_onboarding_portal.kv')
+
+
+class LoginScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        conn = sqlite3.connect('first_db.db')
+        c = conn.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS customers (  
+                email TEXT,
+                password TEXT)
+            """)
+        #default user
+        c.execute(
                     '''
                     INSERT INTO customers (email, password)
                     VALUES ("doctor", "apple")
                     '''
                 )
-            conn.commit()
-            conn.close()
-            return Builder.load_file('etbx.kv') 
-	
-    def login(self): 
+        conn.commit()
+        conn.close()
+    
+    def login(self, screen_manager): 
             conn = sqlite3.connect('first_db.db')
             c = conn.cursor()	
-            email = self.root.ids.email_input.text
-            password = self.root.ids.password_input.text
+            email = self.ids.email_input.text
+            password = self.ids.password_input.text
 
             if email != '' and password != '':
                 c.execute('SELECT password FROM customers WHERE email =?', [email])
@@ -40,8 +43,13 @@ class ETBxApp(MDApp):
                 conn.close()  
                 if account: 
                     if account[0] == password: 
-                        popup = Popup(title='Test popup', content=Label(text='Login Successful'), auto_dismiss=False)
-                        popup.open()
+                        print("Credentials Valid! Switching Screen")  
+                        def switch_screen(dt):
+                            app.root.screen_manager.current = 'signup'
+                        print("Credentials Valid! Switching Screen")
+                        Clock.schedule_once(switch_screen, 0.1)  
+                        # popup = Popup(title='Test popup', content=Label(text='Login Successful'), auto_dismiss=False)
+                        # popup.open()
                     else: 
                         popup = Popup(title='Test popup', content=Label(text='Invalid Password'), auto_dismiss=False)
                         popup.open()
@@ -50,10 +58,6 @@ class ETBxApp(MDApp):
                     popup.open()
             else: 
                 popup = Popup(title='Test popup', content=Label(text='Please enter all the fields'), auto_dismiss=False)
-                popup.open() 
-    
+                popup.open()         
 
 
-
-
-ETBxApp().run()
