82e075d16e34bd8e555b6560eb8b7e96539a4740
Jasper <202110402@fit.edu.ph>
2024-05-05 01:55:40 +0800

    feat(Adjust UI): Created test file for main dashboard for testing purposes
    Created "ETBX_dashb_test.py" to temporarily skip login for testing purposes. Temporarily created a button in main dashboard that reverts to the login page.
    
    Button uses MyButton class for the hover feature

---
+++ src/ETBX_dashb_test.py
@@ -0,0 +1,31 @@
+# Only for tesitng purposes, skips the login process. Run this to test the UI of main dashbvoard!
+
+from kivy.uix.screenmanager import ScreenManager, FadeTransition
+
+from main_dashboard.ETBX_main_dashboard import MainDashboard
+from main_dashboard.ETBx_scan_image import ScanImage
+from main_dashboard.ETBX_view_records import ViewRecords
+from main_dashboard.ETBX_manage_account import ManageAccount
+
+from onboarding_portal.ETBX_login import LoginScreen
+
+from kivymd.app import MDApp
+
+
+class MainApp(MDApp):
+
+    def build(self):
+        self.screen_manager = ScreenManager(transition=FadeTransition())        
+        self.screen_manager.add_widget(MainDashboard(name="main_menu"))
+        self.screen_manager.add_widget(ScanImage(name="scan_img"))
+        self.screen_manager.add_widget(ViewRecords(name="view_rec"))
+        self.screen_manager.add_widget(ManageAccount(name="manage_account"))
+        self.screen_manager.add_widget(LoginScreen(name="login"))
+        return self.screen_manager
+
+
+if __name__ == "__main__":
+    MainApp().run()
+
+
+# helloo
