a493910fabd9588827a0f42a7f69b21fcc7cb00b
Seth Gruspe <201911598@fit.edu.ph>
2024-05-11 13:49:21 +0800

    feat: created UI for results screen
    Changes made to the main dashboard. Manage account is replaced with result screen temporarily for testing purposes. Made corrective changes to each screen so when pressing to the same screen will not cause program termination.
    
    Processing after uploading the image will not work

--- src/ETBX_dashb_test.py
+++ src/ETBX_dashb_test.py
@@ -1,14 +1,13 @@
 # Only for tesitng purposes, skips the login process. Run this to test the UI of main dashbvoard!
 
-from kivy.uix.screenmanager import ScreenManager, FadeTransition
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 from kivy.core.window import Window
 
 from main_dashboard.ETBX_main_dashboard import MainDashboard
 from main_dashboard.ETBx_scan_image import ScanImage
 from main_dashboard.ETBX_view_records import ViewRecords
-from main_dashboard.ETBX_manage_account import ManageAccount
+from main_dashboard.ETBX_scan_results import ScanResult
 
-from onboarding_portal.ETBX_login import LoginScreen
 
 from kivymd.app import MDApp
 
@@ -17,12 +16,11 @@ class MainApp(MDApp):
 
     def build(self):
         Window.maximize()
-        self.screen_manager = ScreenManager(transition=FadeTransition())        
+        self.screen_manager = ScreenManager(transition=NoTransition())        
         self.screen_manager.add_widget(MainDashboard(name="main_dashb"))
         self.screen_manager.add_widget(ScanImage(name="scan_img"))
         self.screen_manager.add_widget(ViewRecords(name="view_rec"))
-        self.screen_manager.add_widget(ManageAccount(name="manage_account"))
-        self.screen_manager.add_widget(LoginScreen(name="login"))
+        self.screen_manager.add_widget(ScanResult(name="scan_result"))
         return self.screen_manager
 
 
--- src/main_dashboard/ETBX_scan_results.py
+++ src/main_dashboard/ETBX_scan_results.py
@@ -1,17 +1,17 @@
 from kivy.uix.screenmanager import Screen   
 from kivy.lang import Builder
-from components.classifier_only import load_model_from_file, predict
+#from components.classifier_only import load_model_from_file, predict
 
 # Load the trained model
-model = load_model_from_file('assets\ml-model\efficientNetB7_v0-5.h5')
+# model = load_model_from_file('assets\ml-model\efficientNetB7_v0-5.h5')
 
 Builder.load_file("main_dashboard/maindash_kivy_files/etbx_scan_res.kv")
 
 class ScanResult(Screen):
-    def update_result(self, image_path):
-        self.ids.res_img.source = image_path
-        predicted_class, predicted_score = predict(model, image_path)
-        scoree = float(predicted_score) * 100
-        self.ids.result_class.text = "Predicted Class: " + predicted_class
-        self.ids.result_perc.text = "Predicted Score: " + str(scoree) + " %"
+    # def update_result(self, image_path):
+    #     self.ids.res_img.source = image_path
+    #     predicted_class, predicted_score = predict(model, image_path)
+    #     scoree = float(predicted_score) * 100
+    #     self.ids.result_class.text = "Predicted Class: " + predicted_class
+    #     self.ids.result_perc.text = "Predicted Score: " + str(scoree) + " %"
     pass
