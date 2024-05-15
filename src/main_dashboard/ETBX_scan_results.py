85119e131885bdacaf9254fc2c79c52275324da1
vnzrbr <202110530@fit.edu.ph>
2024-05-15 22:25:13 +0800

    feat: Added Working Scan Result Image and Percentage
    integrated scan image function to reworked results page

--- src/main_dashboard/ETBX_scan_results.py
+++ src/main_dashboard/ETBX_scan_results.py
@@ -1,17 +1,23 @@
 from kivy.uix.screenmanager import Screen   
 from kivy.lang import Builder
-#from components.classifier_only import load_model_from_file, predict
+
+from components.classifier_only import load_model_from_file, predict
 
 # Load the trained model
-# model = load_model_from_file('assets\ml-model\efficientNetB7_v0-5.h5')
+model = load_model_from_file('assets\ml-model\efficientNetB7_v0-5.h5')
 
 Builder.load_file("main_dashboard/maindash_kivy_files/etbx_scan_res.kv")
 
 class ScanResult(Screen):
-    # def update_result(self, image_path):
-    #     self.ids.res_img.source = image_path
-    #     predicted_class, predicted_score = predict(model, image_path)
-    #     scoree = float(predicted_score) * 100
-    #     self.ids.result_class.text = "Predicted Class: " + predicted_class
-    #     self.ids.result_perc.text = "Predicted Score: " + str(scoree) + " %"
+    def update_result(self, image_path):
+        self.ids.res_img.source = image_path
+        predicted_class, predicted_score = predict(model, image_path)
+        if predicted_class == "tuberculosis":
+            predicted_class = "TB Positive: "
+        else:
+            predicted_class = "Non-TB: "    
+        scoree = round(float(predicted_score) * 100, 2)
+        #self.ids.percent_bar.size = self.parent.width * 0.35 * predicted_score, self.parent.height * 0.03
+        self.ids.result_classnPerc.text = predicted_class + str(scoree) + " %"
+
     pass
