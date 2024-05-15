2072715f10b133d4f336bf5b1f0c8a9bf2508170
Jasper <202110402@fit.edu.ph>
2024-05-16 07:32:06 +0800

    feat: Update dependencies.txt and add image processing components.
    Base function of grad CAM added. For integration to UI.

--- src/ETBX_widget_router.py
+++ src/ETBX_widget_router.py
@@ -1,8 +1,6 @@
 from kivy.uix.screenmanager import ScreenManager, FadeTransition
 from kivy.core.window import Window
 
-from onboarding_portal.ETBX_login import LoginScreen
-from onboarding_portal.ETBX_signup import SignupScreen
 
 from main_dashboard.ETBX_main_dashboard import MainDashboard
 from main_dashboard.ETBx_scan_image import ScanImage
@@ -17,8 +15,6 @@ class MainApp(MDApp):
     def build(self):
         Window.maximize()
         self.screen_manager = ScreenManager(transition=FadeTransition())
-        self.screen_manager.add_widget(LoginScreen(name='login'))
-        self.screen_manager.add_widget(SignupScreen(name='signup'))
         self.screen_manager.add_widget(MainDashboard(name='main_menu'))
         self.screen_manager.add_widget(ScanImage(name='scan_img'))
         self.screen_manager.add_widget(ViewRecords(name='view_rec'))
