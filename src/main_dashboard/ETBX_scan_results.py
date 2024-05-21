58335d3ae5a5100aa779cfa2186efeeed66abd36
vnzrbr <202110530@fit.edu.ph>
2024-05-21 20:36:04 +0800

    feat: Function for changing images using provided buttons
    added change_img function which will be used for the switching of images within the scan_results screen
    
    uses dummy images

--- src/main_dashboard/ETBX_scan_results.py
+++ src/main_dashboard/ETBX_scan_results.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen   
 from kivy.lang import Builder
+from kivymd.uix.button import MDRaisedButton   
 
 from components.classifier_only import load_model_from_file, predict
 
@@ -8,9 +9,13 @@ model = load_model_from_file('assets\ml-model\efficientNetB7_v0-5.h5')
 
 Builder.load_file("main_dashboard/maindash_kivy_files/etbx_scan_res.kv")
 
+
+
 class ScanResult(Screen):
     def update_result(self, image_path):
-        self.ids.res_img.source = image_path
+        global xrayRes
+        xrayRes = image_path
+        self.ids.res_img.source = xrayRes
         predicted_class, predicted_score = predict(model, image_path)
         if predicted_class == "tuberculosis":
             predicted_class = "TB Positive: "
@@ -20,4 +25,14 @@ class ScanResult(Screen):
         #self.ids.percent_bar.size = self.parent.width * 0.35 * predicted_score, self.parent.height * 0.03
         self.ids.result_classnPerc.text = predicted_class + str(scoree) + " %"
 
-    pass
+    def change_img(self, instance):
+        if instance == self.ids.x_ray:
+            self.ids.res_img.source = xrayRes
+        elif instance == self.ids.grad_cam:
+            #dummy
+            self.ids.res_img.source = 'assets/heat.png'
+        elif instance == self.ids.pre_proc: 
+            #dummy
+            self.ids.res_img.source = 'assets/xray.jpg'        
+        else:
+            pass
