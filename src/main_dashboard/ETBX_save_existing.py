7d68f44157701a47ecf4813182adef426e54dace
Seth Gruspe <201911598@fit.edu.ph>
2024-05-21 11:10:50 +0800

    feat: created pages for saving scan results to existing patient and to a new patient
    patient existing page are still hard coded and save functions are not yet working. Both pages to be added for transition

---
+++ src/main_dashboard/ETBX_save_existing.py
@@ -0,0 +1,9 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+from kivy.app import App
+from kivymd.app import MDApp
+
+class SaveExisting(MDApp): 
+    pass
+
+SaveExisting().run()
