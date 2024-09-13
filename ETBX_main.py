10d73997153f4e8a670f16bbb2f562841a18eb8b
Jasper <202110402@fit.edu.ph>
2024-09-13 13:27:39 +0800

    docs: Update software version to 1.2

--- ETBX_main.py
+++ ETBX_main.py
@@ -18,7 +18,7 @@ from src.components.core_functions.resource_path import resource_path
 
 from kivymd.app import MDApp
 
-SOFTWARE_VERSION = '1.1'
+SOFTWARE_VERSION = '1.2'
 
 class MainApp(MDApp):
     Config.set('kivy', 'exit_on_escape', '0')
